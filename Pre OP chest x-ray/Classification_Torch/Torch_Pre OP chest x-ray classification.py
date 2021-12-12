0ab1c763f676db8639fb091eac0ba20a16ecb794
DukJu <donggrui518@gmail.com>
2021-12-12 15:45:46 +0900

    torch dataloader

--- Pre OP chest x-ray/Classification_Torch/Torch_Pre OP chest x-ray classification.py
+++ Pre OP chest x-ray/Classification_Torch/Torch_Pre OP chest x-ray classification.py
@@ -6,30 +6,45 @@ USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
 
 !pip install pydicom
+!pip install efficientnet_pytorch
 
+import glob
 import os
+import os.path as osp
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+%matplotlib inline
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+from torchvision import models, transforms
+
+import PIL
+from PIL import Image
 import cv2
 import pydicom
 
-from torchvision import transforms, datasets
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+from efficientnet_pytorch import EfficientNet
 
-from torch.utils.data import Dataset
-from torchvision.transforms import ToTensor
+# transform
+class ImageTransform():
+    def __init__(self):
+        self.data_transform = transforms.Compose([transforms.ToTensor(),
+                                                  transforms.Normalize(0.485, 0.229)])
 
-normal_path = "/Normal"
-LULz_path = "/TB/LULz"
-RULz_path = "TB/RULz"
+    def __call__(self, img):
+        return self.data_transform(img)
 
-class DatasetTrain(torch.utils.data.Dataset):
-    def __init__(self, normal_path, LULz_path, RULz_path):
+# dataset
+class CXRDataset(data.Dataset):
+    def __init__(self, normal_path, LULz_path, RULz_path, transform=None):
+        self.transform = transform
+
         self.normal_path = normal_path
         self.LULz_path = LULz_path
         self.RULz_path = RULz_path
@@ -62,40 +77,45 @@ class DatasetTrain(torch.utils.data.Dataset):
         self.TB_paths = self.TB_LULz_paths + self.TB_RULz_paths
         self.train_paths = self.normal_paths + self.TB_paths
 
-        self.num_trains = len(self.train_paths)
-
     def __len__(self):
-        return self.num_trains
+        return len(self.train_paths)
 
-    def __getitem__(self):
+    def __getitem__(self, index):
+        # index번째 CXR 로드
+        img_path = self.train_paths[index]
+        img_origin = pydicom.read_file(img_path)
+        img_arr = img_origin.pixel_array
+        img_re = cv2.resize(img_arr, (1024,1024), interpolation=cv2.INTER_LINEAR)
+        img = img_re.astype(np.float32)
 
-        # labels
-        labels_N = [np.array([1,0]) for _ in range(len(self.normal_paths))]
-        labels_TB = [np.array([0,1]) for _ in range(len(self.TB_paths))]
+        # transforms
+        img_transformed = self.transform(img)
 
-        train_labels = labels_N + labels_TB
+        # label
+        if "TB" in img_path:
+            label = 1
+        else:
+            label = 0
 
-        # shuffle
-        s = np.arange(len(self.train_paths))
-        np.random.shuffle(s)
+        return img_transformed, label
 
-        train_paths = np.array(self.train_paths)
-        train_paths = train_paths[s]
-        train_paths = list(train_paths)
+# Dataset
 
-        train_labels = np.array(train_labels)
-        train_labels = train_labels[s]
+normal_path = "Normal"
+LULz_path = "LULz"
+RULz_path = "RULz"
 
-        img_list = []
+train_dataset = CXRDataset(
+    normal_path, LULz_path, RULz_path, transform=ImageTransform()
+)
 
-        # IMG load
-        for l in range(len(train_paths)):
-            img = pydicom.read_file(train_paths[l])
-            arr = img.pixel_array
-            re_img = cv2.resize(arr, (self.img_h, self.img_w), interpolation=cv2.INTER_AREA)
-            img_list.append(re_img)
+# 미니 배치 크기 지정
+batch_size = 32
 
-        return (img_list, train_labels)
+# 데이터 로더 작성
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size = batch_size, shuffle = True
+)
 
 # 우선 데이터불러오는 class까지 만듦
 # --> dataloader만든 후 efficientnet으로 학습해보기
